fe9a31e4044e15df573dbe9ad3c16173dafa5179
mammadzadaa <mammedzadea@itstep.edu.az>
2020-08-31 11:54:41 +0400

    lesson_31_08_20

---
+++ C#/lesson_31_08_20/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lesson_31_08_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
